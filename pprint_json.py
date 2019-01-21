32de73379a41cbe93540355456073b0d28c50a75
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-21 21:06:03 +0400

    Fix notices

--- pprint_json.py
+++ pprint_json.py
@@ -3,26 +3,26 @@ import sys
 
 
 def load_data(filepath):
-    file_pointer = open(filepath, 'r')
-    file_content = file_pointer.read()
-    file_pointer.close()
-    return file_content
+    with open(filepath) as filepointer:
+        return filepointer.read()
 
 
 def pretty_print_json(string):
-    try:
-        json_data = json.loads(string)
-        print(json.dumps(json_data, sort_keys=True, indent=4))
-    except ValueError:
-        print(string)
+    json_data = json.loads(string)
+    print(json.dumps(json_data, sort_keys=True, indent=4))
 
 
-def main(file_path):
-    pretty_print_json(load_data(file_path))
-
-
-if __name__ == '__main__':
+def main():
     if len(sys.argv) != 2:
         print('You have to pass filename as an argument')
     else:
-        main(sys.argv[1])
+        try:
+            pretty_print_json(load_data(sys.argv[1]))
+        except FileNotFoundError:
+            print('Please use correct file name')
+        except json.JSONDecodeError:
+            print("Not a correct json")
+
+
+if __name__ == '__main__':
+    main()
